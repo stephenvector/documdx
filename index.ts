2592cfc1134d351b822c8b774d1adcd8064dd8c1
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2020-02-05 22:46:30 -0500

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,34 @@
+import get from "lodash/lib/get"
+import set from "lodash/lib/set"
+
+class Store {
+  private listenerFunctions: (path: string, value: any) => void
+  private value: {[key: string]: any}
+
+  registerListener(path: string, func: (path: string, value: any) => void) {
+    if (!Array.isArray(this.value[path])) {
+      this.value[path] = [];
+    }
+    
+    this.value[path].push(func);
+  }
+  
+  setValue(path: string, newValue: string) {
+    set(path,newValue);
+    if (!Array.isArray(this.value[path])) {
+      return
+    }
+    
+    this.value[path].forEach(callbackFunction => {
+      callbackFunction(path, newValue);
+    });
+  }
+}
+
+const store = new Store();
+
+function a(eventName) {
+  console.log(newValue);
+}
+
+store.registerListener('a', a);
